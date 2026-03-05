589ca98306feecf51376d531ae98d3c5a814c32b
Han Se Kendall <hskendall2005@outlook.com>
2026-03-05 14:57:01 +0000

    Basic UI and random agent script for POC

--- main.py
+++ main.py
@@ -1,17 +1,105 @@
-from src.agent import Agent, Player
+from src.agent import *
 from src.board import Board
 from src.rewards import calculate_reward
 
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton, QHBoxLayout
+from PySide6.QtCore import Qt
+import sys
+from typing import Literal
+
 #game loop
 board = Board()
-players = (Player(1), Player(2))
+players = (Player(), RL_Agent())
 
 game_running = True
 
-while game_running:
-  for player in players:
-    print(board.base)
-    player.move(board, player.agent)
-    if(board.check_win()):
-      game_running = False
-      break
+class GameWindow(QMainWindow):
+  square_size = 64
+  margin = 4
+
+  def __init__(self, board : Board, board_size : int, players : tuple[Agent, Agent]):
+    super().__init__()
+
+    self.setGeometry(100, 100, 1024, 768)
+    self.setWindowTitle("Gomoku")
+    central_widget = QWidget()
+    sz = (GameWindow.square_size + GameWindow.margin) * board_size - GameWindow.margin
+    central_widget.setFixedSize(sz, sz)
+
+    self.board_render = QGridLayout(central_widget)
+    self.setLayout(self.board_render)
+    self.btns : dict[tuple[int, int], QPushButton] = dict()
+
+    for i in range(board_size):
+      for j in range(board_size):
+        btn = QPushButton()
+        btn.setFixedSize(GameWindow.square_size,GameWindow.square_size)
+        btn.clicked.connect(self.board_click(i,j))
+
+        self.board_render.addWidget(btn, i, j)
+        self.btns[(i,j)] = btn
+    
+    self.setCentralWidget(central_widget) 
+
+
+
+    self.board = board
+    self.board_sz = board_size
+    self.awaiting_board_input = False
+    self.current_player : Literal[0,1] = 0
+    self.players = players
+
+    self.update_game()
+
+  def board_click(self, i, j):
+    def click(_, x : int = i, y : int = j):
+      if self.awaiting_board_input:
+        self.board.update((x,y),self.current_player+1)
+        self.awaiting_board_input = False
+        self.player_complete()
+      else:
+        print("Not expecting input")
+    return click
+  
+  def update_game(self):
+    cmd = self.players[self.current_player].command(
+      self.board, calculate_reward(board.base, self.current_player + 1))
+    if cmd:
+      self.board.update(cmd, self.current_player + 1)
+      self.player_complete()
+    else:
+      self.awaiting_board_input = True
+
+  def player_complete(self):
+    # Update board render
+    for i in range(self.board_sz):
+      for j in range(self.board_sz):
+        if self.board.base[i,j] == 0: continue
+        if not self.btns[(i,j)].isEnabled(): continue
+        
+        self.btns[(i,j)].setStyleSheet(
+          "QPushButton { background-color: " + 
+          ("red;\n" if self.current_player == 0 else "blue;\n") +
+          """
+                color: black;
+                border-radius: 8px;
+                font-size: 16px;
+            }
+            QPushButton:hover {background-color: grey;}
+            QPushButton:pressed {background-color: grey;}""")
+        self.btns[(i,j)].setDisabled(True)
+        
+    self.current_player = (self.current_player + 1) % 2
+    if self.board.check_win():
+      self.close()
+    else:
+      self.update_game()
+
+try:
+  app = QApplication()
+  window = GameWindow(board, 9, players)
+
+  window.show()
+  sys.exit(app.exec())
+except Exception as e:
+    print(f"Application error: {e}")
--- src/agent.py
+++ src/agent.py
@@ -1,25 +1,44 @@
 from src.board import Board
 
-class Agent:
-    def __init__(self):
-        pass
-    def command(self, board : Board, reward):
-        return False #placeholder
+from abc import ABC, abstractmethod
+from random import randint
 
-class Player:
-    def __init__(self, index):
-        self.player_num = index
-        self.agent = Agent()
-        self.set_reward()
-    def set_reward(self):
-        self.reward_struct = 0 #placeholder
-        pass
-    def move(self, board : Board, agent):
-        if agent.command(board, self.reward_struct):
-            print("placeholder")
-        else:
-            #for human play
-            col = int(input("enter column: "))
-            row = int(input("enter row: "))
-            pos = (col, row)
-            board.update(pos, self.player_num)
+class Agent(ABC):
+  def __init__(self):
+    pass
+  
+  @abstractmethod
+  def command(self, board : Board, reward) -> bool | tuple[int, int]:
+    return False #placeholder
+    
+class Player(Agent):
+  def command(self, board : Board, reward) -> bool | tuple[int, int]:
+    return False
+  
+class RL_Agent(Agent):
+  def command(self, board : Board, reward) -> bool | tuple[int, int]:
+    while(1):
+      pos = (randint(0,board.base.shape[0]-1), randint(0,board.base.shape[0]-1))
+
+      if board.base[pos] == 0:
+        return pos
+    return False
+
+
+# class Player:
+#     def __init__(self, index):
+#         self.player_num = index
+#         self.agent = Agent()
+#         self.set_reward()
+#     def set_reward(self):
+#         self.reward_struct = 0 #placeholder
+#         pass
+#     def move(self, board : Board, agent):
+#         if agent.command(board, self.reward_struct):
+#             print("placeholder")
+#         else:
+#             #for human play
+#             col = int(input("enter column: "))
+#             row = int(input("enter row: "))
+#             pos = (col, row)
+#             board.update(pos, self.player_num)
