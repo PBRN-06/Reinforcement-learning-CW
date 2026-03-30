948c67c5a48257b736db0ca51294b6cb4547a6b7
Han Se Kendall <hskendall2005@outlook.com>
2026-03-30 15:42:27 +0100

    cleanup

--- gomoku_rl/env/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Gomoku environment: board, rules, reward wrapper."""
-
-from gomoku_rl.env.board import GomokuBoard
-from gomoku_rl.env.reward_wrapper import RewardWrapper
-
-__all__ = ["GomokuBoard", "RewardWrapper"]
--- gomoku_rl/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Models: CNN policy network and optional MCTS."""
-
-from gomoku_rl.models.policy_net import GomokuPolicyNet
-from gomoku_rl.models.mcts import MCTS
-
-__all__ = ["GomokuPolicyNet", "MCTS"]
